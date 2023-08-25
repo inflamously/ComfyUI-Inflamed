eb91239980df5a87994776e6c03c286592d0c6f5
inflamously <david.dembinski.1996@gmail.com>
2023-08-25 18:39:13 +0200

    feat: implements custom nodes and refresh ability when editing nodes

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -1066,13 +1066,15 @@ export class ComfyApp {
 			this.graph.setDirtyCanvas(true, false);
 			delete this.nodePreviewImages[this.runningNodeId]
 		});
-
+		// TODO: UI Update
 		api.addEventListener("executed", ({ detail }) => {
 			this.nodeOutputs[detail.node] = detail.output;
 			const node = this.graph.getNodeById(detail.node);
 			if (node) {
 				if (node.onExecuted)
 					node.onExecuted(detail.output);
+
+				this.updateNode(node, detail);
 			}
 		});
 
@@ -1192,8 +1194,8 @@ export class ComfyApp {
 			this.loadGraphData();
 		}
 
-		// Save current workflow automatically
-		setInterval(() => localStorage.setItem("workflow", JSON.stringify(this.graph.serialize())), 1000);
+        // Save current workflow automatically
+        setInterval(async () => await this.saveWorkflow(), 1000);
 
 		this.#addDrawNodeHandler();
 		this.#addDrawGroupsHandler();
@@ -1206,6 +1208,9 @@ export class ComfyApp {
 		await this.#invokeExtensionsAsync("setup");
 	}
 
+	async saveWorkflow() {
+        localStorage.setItem("workflow", JSON.stringify(this.graph.serialize()));
+    }
 	/**
 	 * Registers nodes with the graph
 	 */
@@ -1664,11 +1669,23 @@ export class ComfyApp {
 		this.extensions.push(extension);
 	}
 
-	/**
-	 * Refresh combo list on whole nodes
-	 */
-	async refreshComboInNodes() {
+    /**
+     * Refresh combo list on whole nodes
+     * @param {HTMLElement} button
+     */
+	async refreshComboInNodes(button) {
+		if (button.getAttribute("disabled")) {
+			// Do not allow multiple refreshes
+			return;
+		}
+		button.setAttribute("disabled", true);
+		// Reload custom node modules under custom_nodes
+		const customNodeData = await api.getCustomNodes();
+
+		// Reload combobox
 		const defs = await api.getNodeDefs();
+        LiteGraph.clearRegisteredTypes();
+        await this.registerNodesFromDefs(defs);
 
 		for(let nodeNum in this.graph._nodes) {
 			const node = this.graph._nodes[nodeNum];
@@ -1692,6 +1709,8 @@ export class ComfyApp {
 				}
 			}
 		}
+
+		button.removeAttribute("disabled");
 	}
 
 	/**
@@ -1704,6 +1723,25 @@ export class ComfyApp {
 		this.lastExecutionError = null;
 		this.runningNodeId = null;
 	}
+
+	/**
+     * Update Node UI Based on node state data
+     * TODO: Better Idea than just plain impl into App?
+     */
+    updateNode(node, detail) {
+        switch (node.type) {
+            case "DebugNode":
+                const {texts} = detail.output
+                if (texts !== undefined && texts.length > 0) {
+                    node.title = texts[0].substring(0, 16);
+                    node.widgets[0].value = texts[0]
+                }
+                break;
+			case "DebugCond":
+				console.log(detail)
+				break;
+        }
+    }
 }
 
 export const app = new ComfyApp();
