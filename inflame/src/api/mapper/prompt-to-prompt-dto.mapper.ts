19bf0b16ae02f600214ef9c3206ce29e1d8720c5
inflamously <david.dembinski.1996@gmail.com>
2023-11-20 23:54:46 +0100

    feat: removed socket from prompt since it is not a part of it

--- inflame/src/api/mapper/prompt-to-prompt-dto.mapper.ts
+++ inflame/src/api/mapper/prompt-to-prompt-dto.mapper.ts
@@ -1,9 +1,13 @@
 import {Prompt} from "../../+state/prompt/prompt.ts";
 import {PromptDTO} from "../dto/prompt-node.dto.ts";
 import {workflowToWorkflowDtoMapper} from "./workflow-to-workflow-dto.mapper.ts";
+import {ComfyuiSocket} from "../../+state/socket/comfyui-socket.model.ts";
 
-export const promptToPromptDto = (props: Prompt): PromptDTO => {
-    const {socket, workflow} = props;
+export const promptToPromptDto = (props: {
+    socket: ComfyuiSocket,
+    prompt: Prompt,
+}): PromptDTO => {
+    const {socket, prompt} = props;
 
     const result: PromptDTO = {
         client_id: "",
@@ -11,7 +15,7 @@ export const promptToPromptDto = (props: Prompt): PromptDTO => {
         extra_data: {}
     };
     result.client_id = socket?.clientId ?? undefined
-    result.prompt = workflowToWorkflowDtoMapper(workflow);
+    result.prompt = prompt ? workflowToWorkflowDtoMapper(prompt.workflow) : {};
 
     return result;
 }
