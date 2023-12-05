dc322c4dbcfc1511ab490619fa8c7533cedb9bff
inflamously <david.dembinski.1996@gmail.com>
2023-12-05 23:35:06 +0100

    feat: remove combiner func feat: remove unused app config for now

--- inflame/src/+state/app-config/app-config.selector.ts
+++
@@ -1,6 +0,0 @@
-import {AppState} from "../inflame-store.ts";
-import {AppConfigState} from "./app-config.slice.ts";
-
-export const selectAppState = (state: AppState & AppConfigState): AppConfigState | undefined => {
-    return state.appConfig ? state.appConfig : state;
-}
--- inflame/src/+state/inflame-store.ts
+++ inflame/src/+state/inflame-store.ts
@@ -3,16 +3,6 @@ import {socketSlice, socketSliceName} from "./socket/socket-slice.ts";
 import {nodesSlice, nodesSliceName} from "./data-nodes/data-nodes.slice.ts";
 import {promptsSlice, promptsSliceName} from "./prompt/prompt-workflow/prompts.slice.ts";
 import {useDispatch} from "react-redux";
-import {appConfigSlice, appConfigSliceName} from "./app-config/app-config.slice.ts";
-
-// TODO: Could enable to combine state for X reducer.
-// const testReducerHandler = () => (state, action) => {
-//     console.log(state, action)
-//     return combineReducers({
-//         [socketSliceName]: socketSlice.reducer,
-//         [nodesSliceName]: nodesSlice.reducer,
-//     })(state, action)
-// }
 
 const store = configureStore({
     devTools: true,
@@ -20,7 +10,6 @@ const store = configureStore({
         [socketSliceName]: socketSlice.reducer,
         [nodesSliceName]: nodesSlice.reducer,
         [promptsSliceName]: promptsSlice.reducer,
-        [appConfigSliceName]: appConfigSlice.reducer,
     }),
     middleware: (
         getDefaultMiddleware
