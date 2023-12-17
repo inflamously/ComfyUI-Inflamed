9a8a914a5c8ea8a71382ee2b1fc659d89676d883
inflamously <david.dembinski.1996@gmail.com>
2023-12-17 22:30:20 +0100

    feat(comfyui-socket / prompt): handle socket events using mocks

---
+++ inflame/src/+state/socket/comfyui-socket/comfyui-socket.actions.ts
@@ -0,0 +1,48 @@
+import {createListenerPreparedAction} from "../../action.utils.ts";
+import {
+    ComfyuiExecutedDTO,
+    ComfyuiExecutingDTO,
+    ComfyuiExecutionCachedDTO,
+    ComfyuiExecutionStartDTO, ComfyuiStatusDTO, ComfyuiStatusWithSidDTO
+} from "../../../socket/comfyui/dto/comfyui-message.model.ts";
+
+const executionCached = createListenerPreparedAction("comfyuiSocket/executionCached", (payload: ComfyuiExecutionCachedDTO) => {
+    return {
+        payload
+    }
+})
+
+const executionStart = createListenerPreparedAction(`comfyuiSocket/executionStart`, (payload: ComfyuiExecutionStartDTO) => {
+    return {
+        payload,
+    }
+})
+
+const executing = createListenerPreparedAction('comfyuiSocket/executing', (payload: ComfyuiExecutingDTO) => {
+    return {
+        payload
+    }
+})
+
+const executed = createListenerPreparedAction('comfyuiSocket/executed', (payload: ComfyuiExecutedDTO) => {
+    return {
+        payload,
+    }
+})
+
+const statusEvent = createListenerPreparedAction("comfyuiSocket/statusEvent", (payload: ComfyuiStatusWithSidDTO | ComfyuiStatusDTO) => {
+    return {
+        payload: {
+            queued: payload?.status?.exec_info?.queue_remaining
+        }
+    }
+})
+
+
+export const comfyuiSocketActions = {
+    statusEvent,
+    executionStart,
+    executionCached,
+    executing,
+    executed
+}
