02d93924d1a33303bb3d245c877a3afb5e1cf503
inflamously <david.dembinski.1996@gmail.com>
2023-12-22 03:03:00 +0100

    feat: add new find node by id function

--- inflame/src/+state/prompt/preview-image-pipeline-update.ts
+++
@@ -1,9 +0,0 @@
-import {addNodeToPromptWorkflowUpdateListener} from "./prompt-workflow/prompt-workflow-update.listener.ts";
-
-// TODO: Find good place to live.
-export const subscribePreviewImageNodeUpdate = () => {
-    addNodeToPromptWorkflowUpdateListener((action) => {
-        // TODO: Proper mapping and validation
-        console.log("Preview Image Node received Update :)", action)
-    });
-}
