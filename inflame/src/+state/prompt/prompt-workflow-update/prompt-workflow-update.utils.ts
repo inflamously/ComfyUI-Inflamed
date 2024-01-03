69ff9c1c78b82474b25528dacb4e4703eab213fc
inflamously <david.dembinski.1996@gmail.com>
2024-01-03 23:50:22 +0100

    feat: remove sourceAppendixMatchType chaos

--- inflame/src/+state/prompt/prompt-workflow-update/prompt-workflow-update.utils.ts
+++ inflame/src/+state/prompt/prompt-workflow-update/prompt-workflow-update.utils.ts
@@ -1,5 +1,4 @@
 import {AbstractPromptNode, PipelineNodeUpdateSource} from "@inflame/models";
-import {sameKeys} from "../../../core/object.utils.ts";
 
 export const hasSingleNode = (nodes: Array<AbstractPromptNode | undefined>) => {
     return nodes?.length === 1
@@ -9,14 +8,6 @@ export const sourceContainNodes = (source: PipelineNodeUpdateSource): source is 
     nodes: string[]
 } => {
     return (source?.nodes?.length || 0) > 0;
-}
-
-type KeyedObject<T> = {
-    [k in keyof T]: (T[k] extends Array<unknown> ? T[k] : (T[k] extends Record<string, unknown> ? KeyedObject<T[k]> : undefined)) | undefined
-}
-
-type StripUndefined<T> = {
-    [k in keyof T]: (T[k] extends Array<infer U> ? StripUndefined<U>[] : (T[k] extends Record<string, unknown> ? StripUndefined<T[k]> : unknown))
 }
 
 export const sourceIncludesAppendix = (
@@ -29,8 +20,4 @@ export const sourceIncludesAppendix = (
         console.error("Acquire source did not contain an appendix.")
     }
     return result
-}
-
-export const sourceAppendixMatchType = <ContractType extends Record<string, unknown>>(appendix: Record<string, unknown>, contractType: KeyedObject<ContractType>): appendix is StripUndefined<ContractType> => {
-    return sameKeys(appendix, contractType)
 }
