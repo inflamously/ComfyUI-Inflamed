b245d6b57b67588605df48fa42cd3ecaf31159c3
inflamously <david.dembinski.1996@gmail.com>
2024-02-06 22:28:45 +0100

    feat: rename store to appStore

--- inflame/src/main.tsx
+++ inflame/src/main.tsx
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import {theme} from "./theme.tsx";
 import {Provider} from "react-redux";
-import inflameStore from "./+state/inflame-store.ts";
 import {ChakraProvider} from "@chakra-ui/react";
 import {RouterProvider} from "react-router-dom";
 import {routerPaths} from "./routes/router.utils.ts";
 import {RoutePathsContext} from "./routes/router.utils.ts";
 import {Routing} from "./routes/router.tsx";
+import {appStore} from "@inflame/state";
 
 ReactDOM.createRoot(
     document.getElementById('root')!
 ).render(
     <React.StrictMode>
-        <Provider store={inflameStore}>
+        <Provider store={appStore}>
             <ChakraProvider theme={theme}>
                 <RoutePathsContext.Provider value={routerPaths()}>
                     <RouterProvider router={Routing.router} fallbackElement={<p>Loading...</p>}/>
